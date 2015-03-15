423b7fe53853eb93e51918f2d8bbab395beb25bf
Michael Zwyssig <mzwyssig@3ne.ch>
2015-03-15 23:15:43 +0100

    moved templtes to views

--- assets/app.js
+++ assets/app.js
@@ -322,14 +322,14 @@ app.controller('VoteController', function($scope, $http) {
 app.directive("intro", function(){
   return {
     restrict: "E",
-    templateUrl:"_1_intro.html"
+    templateUrl:"views/1_intro.html"
   };
 })
 
 app.directive("conversation", function(){
   return {
     restrict: "E",
-    templateUrl:"_2_conversation.html"
+    templateUrl:"views/2_conversation.html"
   };
 });
 
@@ -337,14 +337,14 @@ app.directive("conversation", function(){
 app.directive("vote", function() {
   return {
     restrict: "E",
-    templateUrl:"_3_vote.html"
+    templateUrl:"views/3_vote.html"
   }
 });
 
 app.directive("result", function(){
   return {
     restrict: "E",
-    templateUrl:"_4_result.html"
+    templateUrl:"views/4_result.html"
   };
 });
 
